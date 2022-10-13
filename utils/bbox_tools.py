9ff815d43e2f164909d5c3d1e77b706e8fa432a6
inhopp <inhonanananna@gmail.com>
2022-10-14 01:56:56 +0900

    update get_bboxes

--- utils/bbox_tools.py
+++ utils/bbox_tools.py
@@ -2,6 +2,7 @@ import torch
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
+from nms import nms
 
 
 def plot_image(image, boxes):
@@ -29,14 +30,59 @@ def plot_image(image, boxes):
     plt.show()
 
 
-def convert_cellboxes(predictions, S=7):
+def get_bboxes(opt, loader, model, iou_threshold, threshold):
+    device = torch.device("cuda: {}".format(opt.gpu) if torch.cuda.is_available() else "cpu")
+    all_pred_boxes = []
+    all_true_boxes = []
+
+    # make sure model is in eval before get bboxes
+    model.eval()
+    train_idx = 0
+
+    for batch_idx, (x, labels) in enumerate(loader):
+        x = x.to(device)
+        labels = labels.to(device)
+
+        with torch.no_grad():
+            predictions = model(x)
+
+        batch_size = x.shape[0]
+        true_bboxes = cellboxes_to_boxes(labels, opt)
+        bboxes = cellboxes_to_boxes(predictions, opt)
+
+        for idx in range(batch_size):
+            nms_boxes = nms(bboxes[idx], iou_threshold=iou_threshold, threshold=threshold)
+
+            #if batch_idx == 0 and idx == 0:
+            #    plot_image(x[idx].permute(1,2,0).to("cpu"), nms_boxes)
+            #    print(nms_boxes)
+
+            for nms_box in nms_boxes:
+                all_pred_boxes.append([train_idx] + nms_box)
+
+            for box in true_bboxes[idx]:
+                # many will get converted to 0 pred
+                if box[1] > threshold:
+                    all_true_boxes.append([train_idx] + box)
+
+            train_idx += 1
+
+    model.train()
+    return all_pred_boxes, all_true_boxes
+
+
+def convert_cellboxes(predictions, opt):
+    S = opt.S
+    B = opt.B
+    C = opt.num_classes
+
     predictions = predictions.to("cpu")
     batch_size = predictions.shape[0]
-    predictions = predictions.reshape(batch_size, 7, 7, 30)
-    bboxes1 = predictions[..., 21:25]
-    bboxes2 = predictions[..., 26:30]
+    predictions = predictions.reshape(batch_size, S, S, C + B * 5)
+    bboxes1 = predictions[..., C+1:C+5]
+    bboxes2 = predictions[..., C+6:C+10]
     scores = torch.cat(
-        (predictions[..., 20].unsqueeze(0), predictions[..., 25].unsqueeze(0)), dim=0
+        (predictions[..., C].unsqueeze(0), predictions[..., C+5].unsqueeze(0)), dim=0
     )
     best_box = scores.argmax(0).unsqueeze(-1)
     best_boxes = bboxes1 * (1 - best_box) + best_box * bboxes2
@@ -46,9 +92,7 @@ def convert_cellboxes(predictions, S=7):
     w_y = 1 / S * best_boxes[..., 2:4]
     converted_bboxes = torch.cat((x, y, w_y), dim=-1)
     predicted_class = predictions[..., :20].argmax(-1).unsqueeze(-1)
-    best_confidence = torch.max(predictions[..., 20], predictions[..., 25]).unsqueeze(
-        -1
-    )
+    best_confidence = torch.max(predictions[..., C], predictions[..., C+5]).unsqueeze(-1)
     converted_preds = torch.cat(
         (predicted_class, best_confidence, converted_bboxes), dim=-1
     )
@@ -56,8 +100,10 @@ def convert_cellboxes(predictions, S=7):
     return converted_preds
 
 
-def cellboxes_to_boxes(out, S=7):
-    converted_pred = convert_cellboxes(out).reshape(out.shape[0], S * S, -1)
+def cellboxes_to_boxes(out, opt):
+    S = opt.S
+
+    converted_pred = convert_cellboxes(out, opt).reshape(out.shape[0], S * S, -1)
     converted_pred[..., 0] = converted_pred[..., 0].long()
     all_bboxes = []
 
